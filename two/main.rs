89f2ebcd2a706f286ac1dcdfab3460463eabc1e2
Shardul Vaidya <cam.v737@gmail.com>
2024-12-02 06:44:03 +0000

    Definitely smart, with about 50% of incoming requests being smartly resolved

--- two/main.rs
+++ two/main.rs
@@ -49,59 +49,56 @@ fn is_within_relative_range(numbers: &Vec<u8>) -> Result<(), usize> {
     }
 }
 
+fn drop_and_test(input: Vec<u8>, to_drop: usize) -> bool {
+    let mut test_input = input.clone();
+    test_input.remove(to_drop);
+    match (all_sorted(&test_input, 2), is_within_relative_range(&test_input)) {
+        (Ok(_), Ok(_)) => true,
+        _ => false,
+    }
+}
+
 fn main() {
     let results = parse_and_validate("two.input.txt");
     let mut valid_reports = 0;
 
-    let mut damped_reports = 0;
+    let mut damped_slow = 0;
+
+    let mut damped_fast = 0;
 
     results.iter().for_each(|result| {
         match (all_sorted(&result, 2), is_within_relative_range(&result)) {
             (Ok(_), Ok(_)) => { valid_reports += 1; return; },  // All pass
-            (Ok(_), Err(e)) => {
-                // Relative range failed
-                let mut damped_result = result.clone();
-                damped_result.remove(e + 1);
-                match (all_sorted(&damped_result, 2), is_within_relative_range(&damped_result)) {
-                    (Ok(_), Ok(_)) => {damped_reports += 1; return;},  // All pass
-                    _ => {
-                        // if it doesn't work, we don't try to be cute and just brute force it
-                    },
-                }
-            },  // relative range failed
-            (Err(e), Ok(_)) => {
-                // Sorted failed
-                let mut damped_result = result.clone();
-                damped_result.remove(e + 1);
-                match (all_sorted(&damped_result, 2), is_within_relative_range(&damped_result)) {
-                    (Ok(_), Ok(_)) => {damped_reports += 1; return;},  // All pass
-                    _ => {
-                        // if it doesn't work, we don't try to be cute and just brute force it
-                    },
+            (Ok(_), Err(e)) => {  // relative range failed
+                // Try to drop the value at the error index
+                if drop_and_test(result.clone(), e + 1) {
+                    damped_fast += 1;
+                    return;
+                }  
+            },
+            (Err(e), Ok(_)) => { // Sorted failed
+                if drop_and_test(result.clone(), e + 1) {
+                    damped_fast += 1;
+                    return;
                 }
             },  // not sorted
-            (Err(sort_e), Err(range_e)) => {
+            (Err(sort_e), Err(range_e)) => {  // not sorted and some range error
                 if sort_e == range_e {
                     // Both failed at the same index
-                    let mut damped_result = result.clone();
-                    damped_result.remove(sort_e + 1);
-                    match (all_sorted(&damped_result, 2), is_within_relative_range(&damped_result)) {
-                        (Ok(_), Ok(_)) => {damped_reports += 1; return;},  // All pass
-                        _ => {
-                            // if it doesn't work, we don't try to be cute and just brute force it
-                        },
+                    if drop_and_test(result.clone(), sort_e + 1) {
+                        damped_fast += 1;
+                        return;
                     }
                 } else {
-                    // Both failed at different indices
                     let mut damped_result = result.clone();
                     damped_result.remove(sort_e + 1);
                     match (all_sorted(&damped_result, 2), is_within_relative_range(&damped_result)) {
-                        (Ok(_), Ok(_)) => {damped_reports += 1; return;},  // All pass
+                        (Ok(_), Ok(_)) => {damped_fast += 1; return;},  // All pass
                         _ => {
                             damped_result = result.clone();
                             damped_result.remove(range_e + 1);
                             match (all_sorted(&damped_result, 2), is_within_relative_range(&damped_result)) {
-                                (Ok(_), Ok(_)) => {damped_reports += 1; return;},  // All pass
+                                (Ok(_), Ok(_)) => {damped_fast += 1; return;},  // All pass
                                 _ => {
                                     // if it doesn't work, we don't try to be cute and just brute force it
                                 },
@@ -109,7 +106,7 @@ fn main() {
                         },
                     }
                 }
-            },  // not sorted and some range error
+            }, 
         }
 
         // Brute force through all failed lists to see if dropping ANY one value makes a difference in the outcome
@@ -121,7 +118,7 @@ fn main() {
             damped_result.remove(i);
 
             match (all_sorted(&damped_result, 2), is_within_relative_range(&damped_result)) {
-                (Ok(_), Ok(_)) => { damped_reports += 1; return; },  // All pass and we only made one change
+                (Ok(_), Ok(_)) => { damped_slow += 1; return; },  // All pass and we only made one change
                 _ => {
                     // if it doesn't work, we continue
                 },
@@ -129,6 +126,6 @@ fn main() {
         }
     });
 
-    println!("Valid reports: {}, Damped reporst: {}, Passed Reports: {}",
-     valid_reports,  damped_reports, valid_reports + damped_reports);
+    println!("Valid reports: {}, Damped Slow: {}, Damped Fast: {}, Passed Reports: {}",
+     valid_reports,  damped_slow, damped_fast, valid_reports + damped_fast + damped_slow);
 }
